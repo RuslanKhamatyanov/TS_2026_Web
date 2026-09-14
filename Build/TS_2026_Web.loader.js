722cc298b1f111c6645730a8b1e4b1e46f223c4f
Руслан Хаматьянов <ruslan.khamatyanov@fluytstudio.net>
2026-09-14 13:01:59 +0300

    Правка визуала.

--- Build/TS_2026_Web.loader.js
+++ Build/TS_2026_Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 67566772,
+    wasmFileSize: 67626985,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
